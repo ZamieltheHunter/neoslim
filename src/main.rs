0a7c709b1e422b0856bcc55fdb6b960e959b09e0
Chris Barden <christopher.barden@magnetimarelli.com>
2022-09-27 14:47:15 -0400

    Created TODO file and implemented ioctl to grab virtual terminal info

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,24 @@
+#![allow(non_snake_case)]
 use pam_client::{Context, Flag};
 use pam_client::conv_cli::Conversation;
+use nix::sys::ioctl;
+use nix::ioctl_read_bad;
+use core::ffi::c_ushort;
+use nix::fcntl::open;
+
+pub const VT_GETSTATE: u32 = 22019;
+#[repr (C)]
+pub struct vt_stat {
+    v_active: c_ushort,
+    v_signal: c_ushort,
+    v_state: c_ushort
+}
+
+ioctl_read_bad!(getVTState, VT_GETSTATE, vt_stat);
 
 fn startX() {
-    println!("Starting X not actually supported");
-
     //TODO Actually start X11
+    println!("Starting X not actually supported");
 }
 
 fn authenticate() {
@@ -12,7 +26,23 @@ fn authenticate() {
     ()
 }
 
+fn findVirtualTerminal() {
+    // TODO Use IOCTL to get the current active virtual terminal so we know where to start X
+    let termPath = "/dev/tty0";
+    let ttyFD = open(termPath, nix::fcntl::OFlag::O_RDONLY, nix::sys::stat::Mode::empty()).expect("Failed to open tty");
+    println!("Looks like we opened the tty");
+    let mut termInfo = vt_stat {v_active: 0,v_signal: 0,v_state: 0};
+    let mut termPtr: *mut vt_stat = &mut termInfo;
+    let err = unsafe { getVTState(ttyFD, termPtr)};
+    match err {
+        Err(e) => println!("Oofus, errored with code: {}", e),
+        Ok(_) => println!("We got some terminfo it is v_active: {}, v_signal: {}, v_state: {}", termInfo.v_active, termInfo.v_signal, termInfo.v_state),
+    }
+    ()
+}
 fn main() {
+
+    findVirtualTerminal();
     let mut username = String::new();
     let mut context  = Context::new(
         "neoslim",
@@ -43,6 +73,4 @@ fn main() {
         println!("VAR: {}", object);
     }
     let mut session = context.open_session(Flag::NONE).expect("RIP, don't got no open session");
-
-    println!("Hello, world!");
 }
